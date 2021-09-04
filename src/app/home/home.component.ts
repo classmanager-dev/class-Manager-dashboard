d73740361a425994c1a39f6a0a046ff38041067e
rymservicesexpress <b.ferdaous@spherewebservices.com>
2021-09-04 21:18:37 +0100

    add loading spinner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms"
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+
 })
 export class HomeComponent implements OnInit {
   @ViewChild('accountSettings', { static: false }) accountSettings: ModalDirective;
